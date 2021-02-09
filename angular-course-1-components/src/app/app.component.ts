ff4194de847c684f43a0712d021a126096c9e651
Natalia Martian <natalia.martian@gmail.com>
2021-02-09 12:06:55 -0500

    Common Angular built in pipes

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  price = 9.99222222;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
+  startDate = new Date(2000, 0, 1);
+
   //passing the selected course as a parameter to the custom event handler
   onCourseSelected(course:Course) {
     console.log('app component - event selected', course);
